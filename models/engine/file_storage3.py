f63edd5e58342e4b7136c91d43b7ad7c4f124c74
simodevjs <simodevjs@gmail.com>
2024-05-11 01:54:29 +0100

    222

--- models/engine/file_storage3.py
+++ models/engine/file_storage3.py
@@ -2,29 +2,27 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
+
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
-        """Returns a dictionary of models currently in storage optionally filtered by class"""
-        if cls:
-            new_dict = {}
-            for key, val in self.__objects.items():
-                if type(val) == cls:
-                    new_dict[key] = val
-            return new_dict
+    def all(self):
+        """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.__objects[obj.to_dict()['__class__'] + '.' + obj.id] = obj
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {k: v.to_dict() for k, v in self.__objects.items()}
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
@@ -43,19 +41,10 @@ class FileStorage:
                     'Review': Review
                   }
         try:
+            temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    cls_name = val['__class__']
-                    if cls_name in classes:
-                        self.__objects[key] = classes[cls_name](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """Delete obj from __objects if it’s inside"""
-        if obj:
-            obj_key = "{}.{}".format(type(obj).__name__, obj.id)
-            if obj_key in self.__objects:
-                del self.__objects[obj_key]
-                self.save()
