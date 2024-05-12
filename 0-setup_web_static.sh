cfaea4335a13b5abcc8caddf9976063391613673
simodevjs <simodevjs@gmail.com>
2024-05-12 20:32:23 +0100

    dededed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,55 +1,59 @@
 #!/usr/bin/env bash
 
 # Check if Nginx is installed
-if ! dpkg -s nginx &> /dev/null; then
-    echo "Nginx is not installed. Installing Nginx now..."
+if ! command -v nginx &> /dev/null; then
+    # Install Nginx if not already installed
     sudo apt-get update
     sudo apt-get -y install nginx
-    echo "Nginx has been installed."
-else
-    echo "Nginx is already installed."
 fi
 
-# Ask user to continue with the configuration
-read -p "Do you want to continue setting up the web environment? (yes/no): " answer
-if [[ "$answer" != "yes" ]]; then
-    echo "Setup aborted."
-    exit 1
-fi
-
-# Create the required directories
+# Create necessary directories if they don't exist
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 
-# Create a fake HTML file
+# Create a fake HTML file for testing
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create a symbolic link
+# Create symbolic link /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ folder to the ubuntu user and group
+# Give ownership of the /data/ folder to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve the content
-sudo sed -i '/^http {/a \\tinclude /etc/nginx/sites-enabled/*;' /etc/nginx/nginx.conf
-echo "server {
+# Create a new Nginx configuration file for serving web_static content
+config_file="/etc/nginx/sites-available/web_static"
+sudo tee "$config_file" > /dev/null <<EOF
+server {
     listen 80;
+    listen [::]:80;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
     server_name _;
-    location /hbnb_static {
+    # Add a custom header with the server's hostname
+    add_header X-Served-By "$hostname";
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location /hbnb_static/ {
         alias /data/web_static/current/;
     }
-}" | sudo tee /etc/nginx/sites-available/web_static.conf
+}
+EOF
 
-sudo ln -sf /etc/nginx/sites-available/web_static.conf /etc/nginx/sites-enabled/
 
-# Restart Nginx to apply the changes
+# Create a symbolic link to enable the new configuration
+sudo ln -sf "$config_file" /etc/nginx/sites-enabled/web_static
+sudo rm -rf /etc/nginx/sites-enabled/defaults
+# Restart Nginx
 sudo service nginx restart
 
-echo "Setup completed successfully."
 exit 0
