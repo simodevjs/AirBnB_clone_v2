606dd4f38454e6bbf525f52ab7c6ceacf716b3c7
simodevjs <simodevjs@gmail.com>
2024-05-12 20:37:02 +0100

    dedede

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
+# This script sets up web servers for the deployment of web_static. It includes installing Nginx,
+# creating necessary directory structures, setting up a simple HTML file, and configuring Nginx.
 
-# Check if Nginx is installed
+# Check if Nginx is installed and install if it is not
 if ! command -v nginx &> /dev/null; then
-    # Install Nginx if not already installed
     sudo apt-get update
     sudo apt-get -y install nginx
 fi
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+# Create the necessary directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file for testing
 echo "<html>
@@ -20,40 +20,36 @@ echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create symbolic link /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Ensure the symbolic link is correct; remove if exists and recreate
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# Give ownership of the /data/ folder to ubuntu user and group recursively
+# Recursively change ownership of the /data/ directory to the 'ubuntu' user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Create a new Nginx configuration file for serving web_static content
-config_file="/etc/nginx/sites-available/web_static"
-sudo tee "$config_file" > /dev/null <<EOF
+# Configure Nginx to serve the content from the symbolic link
+sudo tee /etc/nginx/sites-available/web_static > /dev/null <<EOF
 server {
-    listen 80;
-    listen [::]:80;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
+    listen 80 default_server;
+    listen [::]:80 default_server;
     server_name _;
-    # Add a custom header with the server's hostname
-    add_header X-Served-By "$hostname";
+    root /var/www/html;
 
-    location / {
-        try_files $uri $uri/ =404;
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html index.htm;
     }
 
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
+    location / {
+        try_files \$uri \$uri/ =404;
     }
 }
 EOF
 
-
-# Create a symbolic link to enable the new configuration
-sudo ln -sf "$config_file" /etc/nginx/sites-enabled/web_static
+# Enable the configuration by linking to the sites-enabled directory
+sudo ln -sf /etc/nginx/sites-available/web_static /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/defaults
-# Restart Nginx
-sudo service nginx restart
+# Reload Nginx to apply the changes
+sudo service nginx reload
 
+# Exit successfully
 exit 0
